6d8ed5274c5c36aa81ae90030657f252396c1020
cmac1223 <chrismac539@gmail.com>
2017-11-16 11:43:28 -0500

    setup get basic frame for post and delete routes in todoController

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public)'));
 
+//fire controllers
+todoController(app);
+
 //list to port
 app.listen(3000);
 console.log('You are listening to port 3000');
